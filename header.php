3256aa2dd6bb7b6b64c9c88e4920a893aba6ac0c
sergeome <sergeome@gmail.com>
2016-05-12 11:20:08 +0300

    Corrected title and focus for nav.

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<base href="<?php bloginfo('template_url') ?>/" />
-	<title>Web-site</title>
+	<title><?php bloginfo('name'); ?></title>
 	<!--	<base href="--><?php //bloginfo('template_url') ?><!--/" />-->
 	<!--Bootstrap CSS-->
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
