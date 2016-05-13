ad9b9228e5fb930f96aca00d22467662abcc1999
sergeome <sergeome@gmail.com>
2016-05-13 15:02:36 +0300

    Added search query filter.

--- functions.php
+++ functions.php
@@ -10,4 +10,27 @@ function SearchFilter($query) {
 }
 add_filter('pre_get_posts','SearchFilter');
 
+function wpse_11826_search_by_title( $search, $wp_query ) {
+   if ( ! empty( $search ) && ! empty( $wp_query->query_vars['search_terms'] ) ) {
+	  global $wpdb;
+
+	  $q = $wp_query->query_vars;
+	  $n = ! empty( $q['exact'] ) ? '' : '%';
+
+	  $search = array();
+
+	  foreach ( ( array ) $q['search_terms'] as $term )
+		 $search[] = $wpdb->prepare( "$wpdb->posts.post_title LIKE %s", $n . $wpdb->esc_like( $term ) . $n );
+
+	  if ( ! is_user_logged_in() )
+		 $search[] = "$wpdb->posts.post_password = ''";
+
+	  $search = ' AND ' . implode( ' AND ', $search );
+   }
+
+   return $search;
+}
+
+add_filter( 'posts_search', 'wpse_11826_search_by_title', 10, 2 );
+
 ?>
